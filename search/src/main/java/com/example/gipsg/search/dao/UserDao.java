b840a0704308663cd6537b5bd8bb5abe5e000d7d
qwertytoki <qwertytoki@gmail.com>
2020-04-26 23:40:02 +0800

    connect dao

--- search/src/main/java/com/example/gipsg/search/dao/UserDao.java
+++ search/src/main/java/com/example/gipsg/search/dao/UserDao.java
@@ -2,17 +2,32 @@ package com.example.gipsg.search.dao;
 
 import com.example.gipsg.search.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+@Repository
 public class UserDao {
 
     @Autowired
     HttpSession session;
 
-    protected List<User> getAllUser() {
+    public List<User> getUsers() {
+        Object userList = session.getAttribute("userDB");
+        if (Objects.isNull(userList)) {
+            userList = getAllUser();
+        }
+        return (List<User>) userList;
+    }
+
+    public void saveUsers(List<User> userList) {
+        session.setAttribute("userDB", userList);
+    }
+
+    private List<User> getAllUser() {
         List<User> userList = new ArrayList<>();
         User user1 = new User(1, "Shuya", "Japan", "12345678", "");
         User user2 = new User(2, "Nook", "Singapore", "44445555", "");
@@ -27,13 +42,5 @@ public class UserDao {
         userList.add(user5);
         userList.add(user6);
         return userList;
-    }
-
-    protected List<User> getUsers(){
-        session.getAttribute("userDB");
-    }
-
-    protected List<User> saveUsers(List<User> userList){
-        session.setAttribute("userDB", userList);
     }
 }
--- search/src/main/java/com/example/gipsg/search/service/UserService.java
+++ search/src/main/java/com/example/gipsg/search/service/UserService.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import com.example.gipsg.search.entity.Search;
 import com.example.gipsg.search.entity.User;
 import com.example.gipsg.search.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserDao userDao;
 
     public List<User> search(Search search) {
         String name = search.getName();
@@ -23,52 +26,33 @@ public class UserService {
         if (!StringUtils.isEmpty(nationality) && nationality.equalsIgnoreCase("4")) {
             return searchSingaporean();
         }
-
         return searchAll();
     }
 
     private List<User> searchNameS() {
-        List<User> userList = new ArrayList();
-        userList.add(user1);
-        userList.add(user3);
+        userDao.getUsers();
+        List<User> userList = userDao.getUsers();
         return userList;
     }
 
     private List<User> searchSingaporean() {
-        List<User> userList = new ArrayList();
-        userList.add(user2);
-        userList.add(user5);
-        userList.add(user6);
+        List<User> userList = userDao.getUsers();
         return userList;
     }
 
     private List<User> searchAll() {
-        List<User> userList = new ArrayList();
-        userList.add(user1);
-        userList.add(user2);
-        userList.add(user3);
-        userList.add(user4);
-        userList.add(user5);
-        userList.add(user6);
-        return userList;
+        return userDao.getUsers();
     }
 
     public User findById(int id) {
         switch (id) {
-            case 1:
-                return new User(1, "Shuya", "Japan", "12345678", "");
-            case 2:
-                return new User(2, "Nick", "Singapore", "44445555", "");
-            case 3:
-                return new User(3, "Shuwei", "China", "77778888", "");
-            case 4:
-                return new User(4, "David", "Sweden", "56785678", "");
-            case 5:
-                return new User(5, "Kinkoon", "Singapore", "12341234", "");
-            case 6:
-                return new User(6, "Pearlyn", "Singapore", "59595959", "");
-            default:
-                throw new IllegalStateException("Unexpected value: " + id);
+            case 1: return new User(1, "Shuya", "Japan", "12345678", "");
+            case 2: return new User(2, "Nick", "Singapore", "44445555", "");
+            case 3: return new User(3, "Shuwei", "China", "77778888", "");
+            case 4: return new User(4, "David", "Sweden", "56785678", "");
+            case 5: return new User(5, "Kinkoon", "Singapore", "12341234", "");
+            case 6: return new User(6, "Pearlyn", "Singapore", "59595959", "");
+            default: throw new IllegalStateException("Unexpected value: " + id);
         }
     }
 }
