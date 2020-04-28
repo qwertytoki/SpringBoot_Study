1ade0fe6d300410867c98ff3de53f8d1e367661b
qwertytoki <qwertytoki@gmail.com>
2020-04-28 17:22:00 +0800

    create edit page

--- search/src/main/java/com/example/gipsg/search/controller/UserController.java
+++ search/src/main/java/com/example/gipsg/search/controller/UserController.java
@@ -36,7 +36,14 @@ public class UserController {
     public String displayView(@PathVariable int id, Model model) {
         User user = userSearchService.findById(id);
         model.addAttribute("user", user);
-        model.addAttribute("hobbies", viewService.getHobbies());
         return "user/view";
+    }
+
+    @RequestMapping("/user/{id}/edit")
+    public String displayEdit(@PathVariable int id, Model model) {
+        User user = userSearchService.findById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("hobbies", viewService.getHobbies());
+        return "user/edit_view";
     }
 }
