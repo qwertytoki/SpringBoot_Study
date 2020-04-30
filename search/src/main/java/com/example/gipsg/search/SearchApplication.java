079c53a47f23755b9bbd5635b3c4eceaaf14698b
qwertytoki <qwertytoki@gmail.com>
2020-04-30 08:43:06 +0800

    layout

--- search/src/main/java/com/example/gipsg/search/SearchApplication.java
+++ search/src/main/java/com/example/gipsg/search/SearchApplication.java
@@ -16,30 +16,32 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @SpringBootApplication
 public class SearchApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchApplication.class, args);
-	}
-	@Bean
-	public LocaleResolver localeResolver() {
-		return new CookieLocaleResolver();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
 
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
-	}
-	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("i18n/messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
-	}
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
